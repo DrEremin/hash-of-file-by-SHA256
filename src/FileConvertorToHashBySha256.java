98885fcba3dbfaae9dff4b795690d173abd72f18
DrEremin <ivaneremin1979@gmail.com>
2021-07-13 13:25:56 +0300

    Add generatorOfPrimes() to class FileConvertorToHashBySha256

--- src/FileConvertorToHashBySha256.java
+++ src/FileConvertorToHashBySha256.java
@@ -1,3 +1,37 @@
 public class FileConvertorToHashBySha256 {
 
+    public final int[] ROUNDED_CONSTANTS;
+    public final int[] PRIMES;
+    private int[] HashValues;
+
+    FileConvertorToHashBySha256() {
+        HashValues = new int[8];
+        PRIMES = generatorOfPrimes(64);
+        ROUNDED_CONSTANTS = new int[64];
+    }
+
+    private int[] generatorOfPrimes(int amountOfPrimes) {
+        int[] primes = new int[amountOfPrimes];
+        primes[0] = 2;
+        int primesSize = 1;
+        boolean flag = false;
+        for (int i = 3, j = 0, number; primesSize < amountOfPrimes; i++, j = 0) {
+            number = (int) Math.sqrt(i);
+            while (number >= primes[j]) {
+                if (i % primes[j] == 0) {
+                    flag = true;
+                    break;
+                } else {
+                    j++;
+                }
+            }
+            if (!flag) {
+                primes[primesSize] = i;
+                primesSize++;
+            } else {
+                flag = false;
+            }
+        }
+        return primes;
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,9 @@
 public class Main {
-
+    public static void main(String[] args) {
+        FileConvertorToHashBySha256 fileConvertorToHashBySha256
+                = new FileConvertorToHashBySha256();
+        for (int i = 0; i < fileConvertorToHashBySha256.PRIMES.length; i++) {
+            System.out.println(fileConvertorToHashBySha256.PRIMES[i]);
+        }
+    }
 }
